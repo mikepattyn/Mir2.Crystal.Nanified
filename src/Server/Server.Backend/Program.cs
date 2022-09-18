678019c6d564f78ea36ae82bf674b51112fe5e3e
Mike Pattyn <mike@givtapp.net>
2022-09-18 15:09:26 +0200

    Addd mediatr to di container

--- src/Server/Server.Backend/Program.cs
+++ src/Server/Server.Backend/Program.cs
@@ -11,6 +11,7 @@ Console.WriteLine("Hello, World!");
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddMediatR(typeof(GetDashboardDataQuery).Assembly);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer()
                 .AddOpenApiDocument();
